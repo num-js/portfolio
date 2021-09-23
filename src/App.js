663bb5a7bfefa2eea6e64c52c066e8d8631f9c51
Numan Ahmed <mdnmnahmed@gmail.com>
2021-09-24 01:21:31 +0530

    Header Component design

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom';
 import './App.scss';
+import Header from './components/Header/Header';
 import ProfilePicture from './components/Profile/ProfilePicture/ProfilePicture';
 import ProfileText from './components/Profile/ProfileText/ProfileText';
 import Skills from './components/Skills/Skills';
@@ -8,6 +9,7 @@ import Skills from './components/Skills/Skills';
 const App = () => {
     return (
         <BrowserRouter>
+            <Header />
             <div className="App">
                 <ProfileText />
                 <ProfilePicture />
