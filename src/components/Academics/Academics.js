bc1c8adfa8248c94fc73d66d67a2f05a1da4f8ac
Numan Ahmed <mdnmnahmed@gmail.com>
2021-09-25 23:04:19 +0530

    Added Academics Section

--- src/components/Academics/Academics.js
+++ src/components/Academics/Academics.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import academicsData from '../../assets/data/academicsData.json';
+import College from './College';
 
 const Academics = () => {
     return (
         <>
-            Academics
+            <section class="text-gray-600 body-font lg:mt-28">
+                <h1 class="text-2xl font-medium text-white title-font mb-2">Academics</h1>
+                <div align="left">
+                    <div class="mt-5">
+                        <div class="-my-8 mt-5 divide-y-2 divide-gray-100">
+                            {
+                                academicsData.map((collegeData, index) => (
+                                    <College collegeData={collegeData} />
+                                ))
+                            }
+                        </div>
+                    </div>
+                </div>
+            </section>
         </>
     );
 }
---
+++ src/components/SharedComponents/ActiveStatusGreenDot/ExternalLink.js
@@ -0,0 +1,9 @@
+const ExternalLink = ({ link }) => {
+    return (
+        <a href={link} target="_BLANK" >
+            <img alt="numan ahmed" src="../icons/external-link.svg" width="24px" />
+        </a>
+    );
+}
+
+export default ExternalLink;
