a0ad197a778d157b6a5a034eb4a3537efa708f43
Numan Ahmed <mdnmnahmed@gmail.com>
2021-09-25 23:39:42 +0530

    Fixed Top gapping issue

--- src/components/Academics/Academics.js
+++ src/components/Academics/Academics.js
@@ -5,11 +5,11 @@ import College from './College';
 const Academics = () => {
     return (
         <>
-            <section class="text-gray-600 body-font lg:mt-28">
+            <section class="text-gray-600 body-font lg:mt-10">
                 <h1 class="text-2xl font-medium text-white title-font mb-2">Academics</h1>
                 <div align="left">
                     <div class="mt-5">
-                        <div class="-my-8 mt-5 divide-y-2 divide-gray-100">
+                        <div class="-my-8 divide-y-2 divide-gray-100">
                             {
                                 academicsData.map((collegeData, index) => (
                                     <College collegeData={collegeData} />
--- src/components/Experiences/Experiences.js
+++ src/components/Experiences/Experiences.js
@@ -5,11 +5,11 @@ import Company from './Company';
 const Experiences = () => {
     return (
         <>
-            <section class="text-gray-600 body-font lg:mt-28">
+            <section class="text-gray-600 body-font lg:mt-10">
                 <h1 class="text-2xl font-medium text-white title-font mb-2">Experiences</h1>
                 <div align="left">
                     <div class="mt-5">
-                        <div class="-my-8 mt-5 divide-y-2 divide-gray-100">
+                        <div class="-my-8 divide-y-2 divide-gray-100">
                             {
                                 experiencesData.map((companyData, index) => (
                                     <Company companyData={companyData} />
