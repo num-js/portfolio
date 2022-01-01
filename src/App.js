d8576aa966a3e0b364a788d3169020281b4d4f11
Numan Ahmed <mdnmnahmed@gmail.com>
2022-01-01 08:13:57 +0530

    Skills Section Design Changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom';
 import './App.scss';
+import './styles.css';
 import Header from './components/Header/Header';
 import RouterComponent from './RouterComponent';
 
