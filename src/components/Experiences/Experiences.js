e658352733a5ad7f39bb0694d4298d435b09ae5d
Numan Ahmed <mdnmnahmed@gmail.com>
2022-05-16 20:50:21 +0530

    Total experience calculator

--- src/components/Experiences/Experiences.js
+++ src/components/Experiences/Experiences.js
@@ -3,6 +3,8 @@ import experiencesData from '../../assets/data/experiencesData.json';
 import MobileExperience from './MobileExperience';
 import useScreenWidth from '../../hooks/useScreenWidth';
 import DesktopViewExperience from './DesktopViewExperience';
+import { generateRandomNumber } from '../../helpers/generateNumber';
+import { getFullDuration } from '../../helpers/getDate';
 
 const Experiences = () => {
     const screenSize = useScreenWidth();
@@ -12,6 +14,15 @@ const Experiences = () => {
             <div class="container mx-auto w-full h-full">
                 <div align="center">
                     <h1 class="text-2xl font-medium text-white title-font mb-2">Experiences</h1>
+
+                    <div class="order-1 rounded-lg shadow-xl px-4 py-2 mt-8"
+                        style={{ boxShadow: `0px 0px 15px 1px rgb(${generateRandomNumber(100, 255)} ${generateRandomNumber(100, 255)} ${generateRandomNumber(100, 255)})`, width: 'fit-content' }}
+                    >
+                        <h3 class="text-l text-white">
+                            {getFullDuration('2020-03-01', '2022-05-05')} <br />
+                            {/* 8 Hrs 20 Min 15 Sec */}
+                        </h3>
+                    </div>
                 </div>
                 <div class="relative wrap overflow-hidden p-10 h-full">
                     {
--- src/helpers/getDate.js
+++ src/helpers/getDate.js
@@ -36,3 +36,31 @@ export const getMonthNameYear = (date) => {
     const splitDate = date.toDateString().split(' ')
     return splitDate[1] + "-" + splitDate[3]
 }
+
+export const getFullDuration = (startDate, endDate) => {
+    startDate = makeDate(startDate);
+    endDate = makeDate(endDate);
+
+    const startMonth = startDate.getFullYear() * 12 + startDate.getMonth();
+    const endMonth = endDate.getFullYear() * 12 + endDate.getMonth();
+    const monthInterval = (endMonth - startMonth) + 1;
+
+    const yearsOfExperience = Math.floor(monthInterval / 12);
+    const monthsOfExperience = monthInterval % 12;
+    const daysOfExperience = new Date(new Date() - startDate);
+
+    let date = "";
+    if (yearsOfExperience) {
+        date += yearsOfExperience;
+        if (monthsOfExperience) {
+            date += " Years " + monthsOfExperience + " Months ";
+        } else {
+            date += " Years"
+        }
+    } else {
+        date += monthsOfExperience + " Months";
+    }
+    daysOfExperience && (date += daysOfExperience.getDate() - 1 + " Days");
+
+    return date;
+}
