f297505fdc2a7fdd3c79a749eebb5b83ca280d08
Numan Ahmed <mdnmnahmed@gmail.com>
2022-06-15 22:53:19 +0530

    created an Utility function for - Scroll to a specific Element (Div)

--- src/helpers/scrollToPosition.js
+++ src/helpers/scrollToPosition.js
@@ -6,5 +6,12 @@
 const scrollToPosition = (yPosition = 0, xPosition = 0) => {
     window.scrollTo(yPosition, xPosition);
 }
-
 export default scrollToPosition;
+
+/**
+ * Scroll to a specific Element (Div) 
+ * @param {String} elementId - Div Id, for scroll to that section  
+ */
+export const scrollToElement = (elementId) => {
+    document.getElementById(elementId).scrollIntoView({ behavior: 'smooth', block: "start" });
+}
