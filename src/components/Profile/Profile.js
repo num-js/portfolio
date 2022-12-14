309ab633b83a5d082e8c88c889ba1bad787fd004
Numan Ahmed <mdnmnahmed@gmail.com>
2022-12-14 23:18:26 +0530

    Nav-Bar top fixed on scroll

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -6,7 +6,7 @@ import ProfileText from './ProfileText/ProfileText';
 const Profile = () => {
     return (
         <>
-            <div className="App" style={{ background: `url("../backgrounds/bg-circles.svg")`, backgroundPositionX: "-150%", backgroundPositionY: "54%", backgroundRepeat: "inherit" }}>
+            <div className="App" style={{ background: `url("../backgrounds/bg-circles.svg")`, backgroundPositionX: "-150%", backgroundPositionY: "54%", backgroundRepeat: "inherit", paddingTop: "80px" }}>
                 <ProfileText />
                 <ProfilePicture />
             </div>
